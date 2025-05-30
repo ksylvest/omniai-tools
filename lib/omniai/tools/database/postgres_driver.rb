7679cc55db5dfd14ff38ee87c0dfbec02ad77e7c
Kevin Sylvestre <kevin@ksylvest.com>
2025-05-30 11:26:03 -0700

    Avoid imports of non-essential libraries

--- lib/omniai/tools/database/postgres_driver.rb
+++ lib/omniai/tools/database/postgres_driver.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "pg"
-
 module OmniAI
   module Tools
     module Database
--- lib/omniai/tools/version.rb
+++ lib/omniai/tools/version.rb
@@ -2,6 +2,6 @@
 
 module OmniAI
   module Tools
-    VERSION = "0.5.2"
+    VERSION = "0.6.0"
   end
 end
