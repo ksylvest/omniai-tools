b1c5033693847ce35ea550d07da2dd7aacb8beca
Dean Singh <dean.singh@gmail.com>
2025-06-02 12:41:00 -0700

    Fix the comment, logging, and class name.

--- lib/omniai/tools/disk/directory_move_tool.rb
+++ lib/omniai/tools/disk/directory_move_tool.rb
@@ -4,10 +4,10 @@ module OmniAI
   module Tools
     module Disk
       # @example
-      #   tool = OmniAI::Tools::Disk::DirectoryCreateTool.new(root: "./project")
-      #   tool.execute(path: "./foo/bar")
-      class DirectoryCreateTool < BaseTool
-        description "Creates a directory."
+      #   tool = OmniAI::Tools::Disk::DirectoryMoveTool.new(root: "./project")
+      #   tool.execute(old_path: "./foo", new_path: "./bar")
+      class DirectoryMoveTool < BaseTool
+        description "Moves a directory from one location to another."
 
         parameter :old_path, :string, description: "a path (e.g. `./old`)"
         parameter :new_path, :string, description: "a path (e.g. `./new`)"
@@ -19,9 +19,8 @@ module OmniAI
         #
         # @return [String]
         def execute(old_path:, new_path:)
-          @logger.info("#{self.class.name}#execute path=#{path.inspect}")
-
-          FileUtils.mv(old_path, new_path)
+          @logger.info("#{self.class.name}#execute old_path=#{old_path.inspect} new_path=#{new_path.inspect}")
+          FileUtils.mv(resolve!(path: old_path), resolve!(path: new_path))
         rescue SecurityError => e
           @logger.error(e.message)
           raise e
